064a00b09c0ff5c7d0b59f5bee0de1c27a96dab5
Memory <memoryeamkijkarn@gmail.com>
2021-05-18 12:39:33 +0700

    Update settings.py

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/root")
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
